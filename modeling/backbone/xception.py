b406652b815d16504408d4b59a917f7b4dd9a4f1
jfzhang95 <jessezhjf@gmail.com>
2018-12-01 15:14:30 +0800

    update

--- modeling/backbone/xception.py
+++ modeling/backbone/xception.py
@@ -20,14 +20,16 @@ class SeparableConv2d(nn.Module):
 
         self.conv1 = nn.Conv2d(inplanes, inplanes, kernel_size, stride, 0, dilation,
                                groups=inplanes, bias=bias)
-        self.bn = BatchNorm(inplanes)
+        self.bn1 = BatchNorm(inplanes)
         self.pointwise = nn.Conv2d(inplanes, planes, 1, 1, 0, 1, 1, bias=bias)
+        self.bn2 = BatchNorm(planes)
 
     def forward(self, x):
         x = fixed_padding(x, self.conv1.kernel_size[0], dilation=self.conv1.dilation[0])
         x = self.conv1(x)
-        x = self.bn(x)
+        x = self.bn1(x)
         x = self.pointwise(x)
+        x = self.bn2(x)
         return x
 
 
@@ -49,28 +51,26 @@ class Block(nn.Module):
         if grow_first:
             rep.append(self.relu)
             rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))
-            rep.append(BatchNorm(planes))
             filters = planes
 
         for i in range(reps - 1):
             rep.append(self.relu)
             rep.append(SeparableConv2d(filters, filters, 3, 1, dilation, BatchNorm=BatchNorm))
-            rep.append(BatchNorm(filters))
 
         if not grow_first:
             rep.append(self.relu)
             rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))
-            rep.append(BatchNorm(planes))
-
-        if not start_with_relu:
-            rep = rep[1:]
 
         if stride != 1:
+            rep.append(self.relu)
             rep.append(SeparableConv2d(planes, planes, 3, 2, BatchNorm=BatchNorm))
 
         if stride == 1 and is_last:
+            rep.append(self.relu)
             rep.append(SeparableConv2d(planes, planes, 3, 1, BatchNorm=BatchNorm))
 
+        if not start_with_relu:
+            rep = rep[1:]
 
         self.rep = nn.Sequential(*rep)
 
