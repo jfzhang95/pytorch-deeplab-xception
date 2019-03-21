24248a76687fd03b272443ac032c0879ac1fdf50
kataoka-s <e5206@gm.kochi-ct.jp>
2019-03-21 20:12:23 +0900

    utils add factory

--- dataloaders/utils.py
+++ dataloaders/utils.py
@@ -27,6 +27,9 @@ def decode_segmap(label_mask, dataset, plot=False):
     elif dataset == 'cityscapes':
         n_classes = 19
         label_colours = get_cityscapes_labels()
+    elif dataset == 'factory':
+        n_classes = 5
+        label_colours = get_factory_labels()
     else:
         raise NotImplementedError
 
@@ -99,3 +102,11 @@ def get_pascal_labels():
                        [64, 0, 128], [192, 0, 128], [64, 128, 128], [192, 128, 128],
                        [0, 64, 0], [128, 64, 0], [0, 192, 0], [128, 192, 0],
                        [0, 64, 128]])
+
+def get_factory_labels():
+    return np.array([
+        [0, 0, 0],
+        [255, 0, 0],
+        [0, 255, 0],
+        [0, 0, 255],
+        [255, 255, 255]])
