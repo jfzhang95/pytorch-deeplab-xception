26865d5108c147b17d1042314a94e5c93960bd6a
Pyjcsx <jf.zhang958@outlook.com>
2019-12-09 16:15:29 +0800

    Update deeplab.py

--- modeling/deeplab.py
+++ modeling/deeplab.py
@@ -22,8 +22,7 @@ class DeepLab(nn.Module):
         self.aspp = build_aspp(backbone, output_stride, BatchNorm)
         self.decoder = build_decoder(num_classes, backbone, BatchNorm)
 
-        if freeze_bn:
-            self.freeze_bn()
+        self.freeze_bn = freeze_bn
 
     def forward(self, input):
         x, low_level_feat = self.backbone(input)
@@ -44,20 +43,33 @@ class DeepLab(nn.Module):
         modules = [self.backbone]
         for i in range(len(modules)):
             for m in modules[i].named_modules():
-                if isinstance(m[1], nn.Conv2d):
-                    for p in m[1].parameters():
-                        if p.requires_grad:
-                            yield p
+                if self.freeze_bn:
+                    if isinstance(m[1], nn.Conv2d):
+                        for p in m[1].parameters():
+                            if p.requires_grad:
+                                yield p
+                else:
+                    if isinstance(m[1], nn.Conv2d) or isinstance(m[1], SynchronizedBatchNorm2d) \
+                            or isinstance(m[1], nn.BatchNorm2d):
+                        for p in m[1].parameters():
+                            if p.requires_grad:
+                                yield p
 
     def get_10x_lr_params(self):
         modules = [self.aspp, self.decoder]
         for i in range(len(modules)):
             for m in modules[i].named_modules():
-                if isinstance(m[1], nn.Conv2d):
-                    for p in m[1].parameters():
-                        if p.requires_grad:
-                            yield p
-
+                if self.freeze_bn:
+                    if isinstance(m[1], nn.Conv2d):
+                        for p in m[1].parameters():
+                            if p.requires_grad:
+                                yield p
+                else:
+                    if isinstance(m[1], nn.Conv2d) or isinstance(m[1], SynchronizedBatchNorm2d) \
+                            or isinstance(m[1], nn.BatchNorm2d):
+                        for p in m[1].parameters():
+                            if p.requires_grad:
+                                yield p
 
 if __name__ == "__main__":
     model = DeepLab(backbone='mobilenet', output_stride=16)
