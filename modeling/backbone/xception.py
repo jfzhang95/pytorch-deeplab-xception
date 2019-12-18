9135e104a7a51ea9effa9c6676a2fcffe6a6a2e6
Pyjcsx <jf.zhang958@outlook.com>
2019-12-18 12:30:10 +0800

    Update xception.py

--- modeling/backbone/xception.py
+++ modeling/backbone/xception.py
@@ -250,7 +250,7 @@ class AlignedXception(nn.Module):
         state_dict = self.state_dict()
 
         for k, v in pretrain_dict.items():
-            if k in model_dict:
+            if k in state_dict:
                 if 'pointwise' in k:
                     v = v.unsqueeze(-1).unsqueeze(-1)
                 if k.startswith('block11'):
